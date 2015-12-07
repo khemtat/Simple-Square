8af1bbd389c3b68fd04c2644990a4bb683c2b0ce
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2015-12-08 04:28:35 +0700

    Removed blank line

--- Simple Square/MapViewController.m
+++ Simple Square/MapViewController.m
@@ -107,20 +107,18 @@
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
     if (annotation == mapView.userLocation)
         return nil;
-    
     MKAnnotationView *view = (MKAnnotationView* )[self.mapView
                                 dequeueReusableAnnotationViewWithIdentifier:@"Place"];
-    
     if (view == nil) {
         view = [[MKAnnotationView alloc] initWithAnnotation:annotation
                                                           reuseIdentifier:@"Place"];
         view.canShowCallout = YES;
         view.calloutOffset = CGPointMake(-5, 5);
         view.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        view.image = [UIImage imageNamed:@"pin"];
     } else {
         view.annotation = annotation;
     }
-    view.image = [UIImage imageNamed:@"pin"];
     return view;
 }
 @end
