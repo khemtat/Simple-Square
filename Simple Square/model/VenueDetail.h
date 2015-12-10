1c628620081bd76c0bb712e8fc23be55e00029da
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2015-12-10 15:58:05 +0700

    Added Venue Detail model

---
+++ Simple Square/model/VenueDetail.h
@@ -0,0 +1,13 @@
+//
+//  VenueDetail.h
+//  Simple Square
+//
+//  Created by Khemtat Lengpaiboon on 12/10/2558 BE.
+//  Copyright © 2558 404 App not found. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VenueDetail : NSObject
+
+@end
---
+++ Simple Square/model/VenueDetail.m
@@ -0,0 +1,13 @@
+//
+//  VenueDetail.m
+//  Simple Square
+//
+//  Created by Khemtat Lengpaiboon on 12/10/2558 BE.
+//  Copyright © 2558 404 App not found. All rights reserved.
+//
+
+#import "VenueDetail.h"
+
+@implementation VenueDetail
+
+@end
