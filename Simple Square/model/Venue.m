a59ac4fbe73d83a4a41f4b0605b1c1cef5a80e87
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2015-12-10 16:05:26 +0700

    Changed Place -> Venue and Implemented extension
    redeclare the property in class extension for using private setter

--- Simple Square/model/Venue.m
+++ Simple Square/model/Venue.m
@@ -1,12 +1,12 @@
 //
-//  Place.m
+//  Venue.m
 //  Simple Square
 //
 //  Created by Khemtat Lengpaiboon on 12/3/2558 BE.
 //  Copyright © 2558 404 App not found. All rights reserved.
 //
 
-#import "Place.h"
+#import "Venue.h"
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
@@ -14,21 +14,20 @@ static  NSString const *kTitle = @"name";
 static  NSString const *kLocation = @"location";
 static  NSString const *kLatitude = @"lat";
 static  NSString const *kLongitude = @"lng";
-static  NSString const *kPlaceDetail = @"placeDetail";
+static  NSString const *kVenueDetail = @"VenueDetail";
 static  NSString const *kVenue = @"venue";
 
-@implementation Place
+@implementation Venue
 
 - (id)initWithDictionary:(NSDictionary *)dict{
     self = [super init];
     if (self) {
         _venue = dict[kVenue];
         self.title = self.venue[kTitle];
-
         _location = self.venue[kLocation];
         _coordinate = [self getLatitudeAndLongitude:self.location];
-        _placeDetail = [[PlaceDetail alloc] initDetailWithDictionary:dict];
-        self.subtitle = self.placeDetail.address;
+        _VenueDetail = [[VenueDetail alloc] initWithDictionary:dict];
+        self.subtitle = self.VenueDetail.address;
     };
     return self;
 }
